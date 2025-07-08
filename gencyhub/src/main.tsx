bb5e2b4971c2a10996a77691bd52bf4e12ada640
rahul <rahulpattusamy@gmail.com>
2025-07-08 15:21:52 +0530

    installed query client

--- gencyhub/src/main.tsx
+++ gencyhub/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
+
+const queryclient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryclient}>
+         <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
