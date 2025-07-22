5563723e440aa547ebe47b2ece7762bfda324c12
rahul <rahulpattusamy@gmail.com>
2025-07-22 08:15:34 +0530

    fixe build error

--- gencyhub/src/components/SearchInput.tsx
+++ gencyhub/src/components/SearchInput.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef } from "react";
 import { CiSearch } from "react-icons/ci";
 import useProductquery from "../productquerystore";
 import { useNavigate } from "react-router-dom";
@@ -9,25 +9,7 @@ const SearchInput = () => {
   const setSearchtext = useProductquery((s) => s.setSearchText);
   const navigate = useNavigate();
 
-  const PlaceholderList = [
-    "Groceries",
-    "Laptops",
-    "Shoes",
-    "Smartphones",
-    "shirts",
-    "Watches",
-    "sunglasses",
-  ];
 
-  const [isText, setText] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setText((p) => (p + 1 >= PlaceholderList.length ? 0 : p + 1));
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, []);
 
   const debounced = useCallback(
     debounce((value: string) => {
@@ -62,7 +44,7 @@ const SearchInput = () => {
           onChange={handleChange}
           className=" transition-all duration-300  border placeholder-black text-black bg-white border-gray-600  focus:ring-black  pl-8 text-lg  focus:outline-none md:rounded-4xl  md:border-white   md:text-xl md:w-100 md:pl-10 md:p-1  w-full rounded-4xl"
           type="text"
-          placeholder={PlaceholderList[isText]}
+          placeholder="Search Products..."
         />
       </div>
     </form>
