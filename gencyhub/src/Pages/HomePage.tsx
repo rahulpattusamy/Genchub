fa151d52aa1760bb18bb1f31a5adc74fae059d05
rahul <rahulpattusamy@gmail.com>
2025-07-22 19:52:22 +0530

    create image carousel

--- gencyhub/src/Pages/HomePage.tsx
+++ gencyhub/src/Pages/HomePage.tsx
@@ -2,15 +2,18 @@ import { useNavigate } from "react-router-dom";
 import categoryimages from "../components/CategoryImage"
 import useCategory from "../hooks/useCategory";
 import useProductquery from "../productquerystore";
+import HeroSection from "../components/BannerImage";
 
 const HomePage = () => {
      const { data } = useCategory();
   const setCategory = useProductquery((s) => s.setCategory);
  const navigate =  useNavigate()
   return (
-     <>
-     <p className=" ml-10 mt-5 md:block text-xl font-medium absolute md:mt-5 md:ml-21 dark:text-white">Product Category</p>
-    <div className="grid grid-cols-2 pl-10 pt-20 gap-y-10 md:p-20 lg:grid-cols-4 lg:p-20 relative">
+     <div className="flex flex-col mt-15 w-full">
+      <div className="md:-ml-0">
+         <HeroSection/>
+      </div>
+    <div className="grid grid-cols-2 pl-10 pt-20 gap-y-10 md:pl-30 lg:grid-cols-4 lg:pl-40 relative">
       {data.map((category, index)=>
       <div className="card2" key={index}>
       <img
@@ -24,7 +27,7 @@ const HomePage = () => {
          <p className="bg-white/10 backdrop-blur-xl border border-white/20 pl-2 absolute -mt-6 rounded-b-xl w-30  md:-mt-6 md:w-55 md:rounded-b-xl"><span className="  font-medium text-black dark:text-white">{category.name}</span></p>
           </div>)}
     </div>
-    </>
+    </div>
   )
 }
 
