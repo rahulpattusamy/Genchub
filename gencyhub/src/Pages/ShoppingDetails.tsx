cf77cde34eabfb187a8b050bf280d99b06c91099
Rahulraj <146526803+rahulpattusamy@users.noreply.github.com>
2025-08-01 10:39:53 +0530

    Update ShoppingDetails.tsx

--- gencyhub/src/Pages/ShoppingDetails.tsx
+++ gencyhub/src/Pages/ShoppingDetails.tsx
@@ -15,7 +15,7 @@ const ShoppingDetails = () => {
   if(user)
   return (
     <>
-      <h1 className="text-5xl text-center ">My Cart</h1>
+      <h1 className="text-5xl text-center dark:text-white ">My Cart</h1>
       <div className="flex flex-col h-full mt-5 justify-between  lg:flex lg:flex-row lg:justify-around lg:mt-10">
         <CartCard />
         <OrderSummary />
